8d395eb831f6fabec43bf4c50ea78285bc4b894c
medidi vv satyanarayana murty <medidi.murty@exilant.com>
2016-12-22 10:23:36 +0530

    minor Modifications

---
+++ ExilantHome/ExilantHome/ExilantHome/MailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MailViewController.swift
+//  ExilantHome
+//
+//  Created by medidi vv satyanarayana murty on 20/12/16.
+//  Copyright © 2016 Medidi  V V  Satyanarayana Murty. All rights reserved.
+//
+
+import UIKit
+
+class MailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
